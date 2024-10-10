f060e1e0107a66fc1ab5790e35af7fd56389c5c8
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2024-10-10 10:21:00 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.Student;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Set<Student> set = new HashSet<>();
+		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i = 0; i<n;i++) {
+			int code = sc.nextInt();
+			set.add(new Student(code));
+		}
+		
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i = 0; i<n;i++) {
+			int code = sc.nextInt();
+			set.add(new Student(code));
+		}
+		
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i = 0; i<n;i++) {
+			int code = sc.nextInt();
+			set.add(new Student(code));
+		}
+		
+		System.out.print("Total students: " + set.size());
+
+	}
+
+}
